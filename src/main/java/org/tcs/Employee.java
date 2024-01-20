8fa503dde27beb850c80afdb45a1ad4fac46da61
Hari150799 <harishwaranmv1015@gmail.com>
2024-01-20 13:48:33 +0530

    harish

--- src/main/java/org/tcs/Employee.java
+++ src/main/java/org/tcs/Employee.java
@@ -2,4 +2,16 @@ package org.tcs;
 
 public class Employee {
 
+	public void empid() {
+	System.out.println("Employee Id Is 1234");
+	 }
+	public void empname() {
+	System.out.println("Employee Name Is Harish");
+		 }
+	public void empaddress() {
+	System.out.println("Employee Address Is Chennai");
+		 }
+	public void empcompany() {
+	System.out.println("Employee Company Is TCS");
+		 }
 }
